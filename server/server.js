daad4a86cd7b6f604f63057c8c1c4244128dd36d
richie369 <richard@agiliztech.com>
2018-01-30 11:41:56 +0530

    init

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 var publicPath = path.join(__dirname,'../public');
 var express = require('express');
 
-var app = express();
+var app = express(); 
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.listen(port, () => {
