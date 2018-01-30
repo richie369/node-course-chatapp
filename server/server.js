fbfbdcc1decf780f2b3721510bcf03fe724719df
richie369 <richard@agiliztech.com>
2018-01-30 15:27:33 +0530

    socket io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,35 @@
 const path = require('path');
-var publicPath = path.join(__dirname,'../public');
-var express = require('express');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
-var app = express(); 
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
-app.listen(port, () => {
-    console.log(`Started up at port ${port}`);
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+  
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+
+  });
 });
 
-module.exports = {app};
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
